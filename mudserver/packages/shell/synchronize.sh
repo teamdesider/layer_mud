b878db444d38386952a21dd1aad46c144d8b195e
tgyf007 <daoyin@xiaofeieideiMac.lan>
2023-05-22 16:02:50 +0800

    modify readme && add shell

---
+++ mudserver/packages/shell/synchronize.sh
@@ -0,0 +1,11 @@
+cd ~/Project/layer/
+
+#config
+cp ~/Project/layer_mud_temp/packages/contracts/mud.config.ts packages/contracts/
+cp ~/Project/layer_mud_temp/packages/client/vite.config.ts packages/client/
+
+#client
+cp ~/Project/layer_mud_temp/packages/client/index.html packages/client/
+cp ~/Project/layer_mud_temp/packages/client/src/index.ts packages/client/src/
+
+#contract
